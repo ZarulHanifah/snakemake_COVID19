5f47139b57cd3b21e185df839ae2b0b8535e3f27
ZarulHanifah <muhammad.zarulhanifah@monash.edu>
2021-10-22 16:57:47 +0800

    mod script relative path

--- workflow/main/src/stats.sh
+++ workflow/main/src/stats.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Sample Average_coverage Percentage_genome Human_contamination" | tr " " "\t"
-for sample in $(ls ALL_FASTQ/ | grep "R1" | sed "s/_.*//"); do
+for sample in $(ls ../../input_folder/ALL_FASTQ/ | grep "R1" | sed "s/_.*//"); do
 	avg_cov=$(samtools depth -r "MN908947.3:1-29903" -a "results/ALL_BAM/""$sample"".primertrim.sorted.bam" | cut -f3 | python -c "import sys; total=sum(int(l) for l in sys.stdin) ; avg=round(total/29903, 2) ; print(avg)")
 	min_cov=$(samtools depth -r "MN908947.3:1-29903" -a "results/ALL_BAM/""$sample"".primertrim.sorted.bam" | cut -f3 | sort -n | head -1)
 	percentage_genome=$(samtools depth -r "MN908947.3:1-29903" -a "results/ALL_BAM/""$sample"".primertrim.sorted.bam" | awk '$3 >= "10" {print $0}' | wc -l | python -c "import sys; percent= int(sys.stdin.read())/29903*100 ; percent = round(percent, 2) ; percent = str(percent) + '%' ; print(percent)")
