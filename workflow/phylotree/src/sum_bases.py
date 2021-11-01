2453b22abee687d9c7487365bda0d146ebac7eaa
ZarulHanifah <muhammad.zarulhanifah@monash.edu>
2021-11-01 10:36:05 +0800

    Added phylotree workflow

--- workflow/phylotree/src/sum_bases.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-import pandas as pd
-import numpy as np
-
-df = pd.read_csv(sys.stdin, sep = "\t",
-                 names = ["chrom", "start", "end"])
-print((df["end"] - df["start"]).sum())
--- workflow/phylotree/src/sum_groupby.awk
+++
@@ -1,6 +0,0 @@
-awk '{
-    arr[$1]+=$2
-   }
-   END {
-     for (key in arr) printf("%s\t%s\n", key, arr[key])
-   }'
